a5fc3ed8270bfceb7a42d14f645076a77e13d959
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-05-09 12:44:19 +0100

    commit tree e4fe5d65f582487f0f2a4bc9ee544077a5982766

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,92 @@
 require_relative './node'
+require 'byebug'
 
 class LinkedList
 
+    attr_accessor :head
+
+    def initialize
+        @head = nil
+    end
+
+    def prepend node
+
+        if !head
+            self.head = node
+            return
+        end
+
+        node.next_node = self.head
+        self.head = node
+
+    end
+
+    def append node
+
+        if !head
+            self.head = node
+            return 
+        end
+
+        last_node = head
+
+        until last_node.next_node == nil
+            last_node = last_node.next_node
+        end
+
+        last_node.next_node = node
+    end
+
+    def delete_head
+        if head
+            self.head = self.head.next_node
+        end
+    end
+
+    def delete_tail
+
+        if !self.head
+            return
+        elsif !self.head.next_node
+            self.head = nil
+            return
+        end
+
+        last_node = self.head
+
+        second_last_node = ''
+
+        until last_node.next_node == nil
+            second_last_node = last_node
+            last_node = last_node.next_node
+        end
+
+        second_last_node.next_node = nil
+    end
+
+    def add_after index, node
+        current_index = 0
+
+        correct_node = self.head
+
+        until index == current_index
+            correct_node = self.head.next_node
+            current_index+= 1
+        end
+
+        node.next_node = correct_node.next_node
+        correct_node.next_node = node
+    end
+
+
+    def search data
+
+        correct_node = self.head
+
+        until correct_node.data == data
+            correct_node = correct_node.next_node
+        end
+
+        correct_node
+    end
 end
